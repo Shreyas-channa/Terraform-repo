fb7c62766577a294f58d76957fa933dbf1b849fb
table <table@gmail.com>
2024-05-10 20:09:40 +0530

    --- Auto Git Commit ---

--- modules/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = "us-east-1" 
-}
-
-resource "aws_instance" "sample" {
-    ami           = var.ami_value
-    instance_type = var.instance_type
-    subnet_id     = var.subnet_id
-}
