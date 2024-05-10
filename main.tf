177967c61cedc81533e782551c7ca180bc752b19
table <table@gmail.com>
2024-05-10 18:42:13 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -1,14 +1,4 @@
-variable "ami_value" {
-  description = "value for the ami"
-}
 
-variable "instance_type" {
-  description = "value for instance type"
-}
-
-variable "subnet_id" {
-  description = "value for the subnet id"
-}
 
 provider "aws" {
     region = "us-east-1" 
